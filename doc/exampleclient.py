9c61bba0741cc35a3d5b8fef62da6ef0b9a32d42
abma <spam@abma.de>
2011-04-25 18:04:28 +0200

    make torrent in result optional

--- doc/exampleclient.py
+++ doc/exampleclient.py
@@ -8,13 +8,13 @@ if len(sys.argv) < 2:
 	print "Usage: "+sys.argv[0]+" FilenameToSearchFor"
 	exit(0)
 
-#proxy = ServerProxy('http://localhost/springfiles/xmlrpc.php')
 proxy = ServerProxy('http://springfiles.com/xmlrpc.php')
 searchstring = {
 #	"category" : "Spring Maps",
 	"logical" : "or",
 	"filename" : sys.argv[1],
 	"springname" : sys.argv[1],
+	"torrent" : True,
 }
 
 pp = pprint.PrettyPrinter(depth=6)
