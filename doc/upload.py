d0b8e36b45d6d3f53da55895c6e898ad1c3ae03b
abma <spam@abma.de>
2011-06-09 10:21:51 -0700

    update upload.py

--- doc/upload.py
+++ doc/upload.py
@@ -7,16 +7,17 @@ import pprint
 import sys
 
 if len(sys.argv) < 6:
-	print "Usage: "+sys.argv[0]+" user pass category filename url"
+	print "Usage: "+sys.argv[0]+" user pass filename url"
 	exit(0)
 
 proxy = ServerProxy('http://springfiles.com/xmlrpc.php')
 data = {
 	"username" : sys.argv[1],
 	"password" : sys.argv[2],
-	"category" : sys.argv[3],
-	"filename" : sys.argv[4],
-	"url" : sys.argv[5],
+	"filename" : sys.argv[3],
+#	"sdp" : "abcdef....", #rapid filename for sdp, optional
+#	"tag" : "zk:stable", #tag in rapid, optional
+	"url" : sys.argv[4],
 }
 
 pp = pprint.PrettyPrinter(depth=6)
